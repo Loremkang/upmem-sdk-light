8258533643dbe36e75dea585bd33fda307b4c3ed
Hongbo Kang <hongbokang@icloud.com>
2024-09-18 17:23:51 +0800

    update in benchmark.

--- src/benchmark/host.cpp
+++ src/benchmark/host.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <string>
 #include <sys/mman.h>
-#include <numa.h>
 
 #include "pim_interface_header.hpp"
 #include "timer.hpp"
@@ -15,11 +14,11 @@ using namespace std;
 enum CommunicationDirection { Host2PIM = 0, PIM2Host = 1 };
 
 void parse_arguments(int argc, char **argv, int &nr_ranks,
-                     string &interfaceType, CommunicationDirection &direction) {
-    if (argc < 4) {
+                     string &interfaceType) {
+    if (argc < 3) {
         fprintf(
             stderr,
-            "Usage: %s <nr_ranks> <Interface Type> <Communication Direction>\n",
+            "Usage: %s <nr_ranks> <Interface Type>\n",
             argv[0]);
         exit(1);
     }
@@ -33,134 +32,144 @@ void parse_arguments(int argc, char **argv, int &nr_ranks,
                 "'UPMEM'.\n");
         exit(1);
     }
-
-    string dir = argv[3];
-    if (dir == "Host2PIM") {
-        direction = CommunicationDirection::Host2PIM;
-    } else if (dir == "PIM2Host") {
-        direction = CommunicationDirection::PIM2Host;
-    } else {
-        fprintf(stderr,
-                "Invalid communication direction. Please enter either "
-                "'Host2PIM' or "
-                "'PIM2Host'.\n");
-        exit(1);
-    }
 }
 
 void TestMRAMThroughput(PIMInterface *interface,
-                        CommunicationDirection direction) {
-    const int MinBufferSizePerDPU = 1 << 10;
-    const int MaxBufferSizePerDPU = 2 << 20;
-    double timeLimitPerTest = 2.0;  // 4 seconds
+                        size_t MaxBufferSizePerDPU, bool flush_cache, bool no_huge_page) {
+    const size_t MinTestSizePerDPU = 1 << 10;
+    const size_t MaxTestSizePerDPU = std::min((size_t)1 << 20, MaxBufferSizePerDPU);
+    const double timeLimitPerTest = 2.0;  // 2 seconds
+    const double earlyStopTimeLimitPerTest = 1.0;
+    const size_t repeatLimitPerTest = 500;
 
     int nrOfDPUs = interface->GetNrOfDPUs();
 
-    uint8_t **dpuBuffer = new uint8_t *[nrOfDPUs];
-    
-
-    if (dynamic_cast<DirectPIMInterface *>(interface) != nullptr) {
-        auto interface_ptr = dynamic_cast<DirectPIMInterface *>(interface);
-        for (int i = 0; i < nrOfDPUs; i ++) {
-            // dpuBuffer[i] = (uint8_t*)numa_alloc_onnode(MaxBufferSizePerDPU, interface_ptr->GetNUMAIDOfDPU(i));
-            // printf("Allocate Buffer %d on NUMA %d\n", i, interface_ptr->GetNUMAIDOfDPU(i));
-            // dpuBuffer[i] = new uint8_t[MaxBufferSizePerDPU];
-            assert((MaxBufferSizePerDPU & 0x1FFFFF) == 0);
-            void* ptr = aligned_alloc(1l << 21, MaxBufferSizePerDPU);                                                                                   
-            madvise(ptr, MaxBufferSizePerDPU, MADV_NOHUGEPAGE);
-            dpuBuffer[i] = (uint8_t*)ptr;
-        }
-    } else {
-        assert(dynamic_cast<UPMEMInterface *>(interface) != nullptr);
-        for (int i = 0; i < nrOfDPUs; i++) {
-            // dpuBuffer[i] = new uint8_t[MaxBufferSizePerDPU];
-            assert((MaxBufferSizePerDPU & 0x1FFFFF) == 0);
-            void* ptr = aligned_alloc(1l << 21, MaxBufferSizePerDPU);
-            madvise(ptr, MaxBufferSizePerDPU, MADV_NOHUGEPAGE);
-            dpuBuffer[i] = (uint8_t*)ptr;
-        }
+    size_t buffer_size = MaxBufferSizePerDPU * nrOfDPUs;
+    size_t cacheline_size = 64;
+    uint8_t *buffer = (uint8_t *)aligned_alloc(1l << 21, buffer_size);
+    if (no_huge_page) {
+        madvise(buffer, buffer_size, MADV_NOHUGEPAGE);
     }
 
-    auto get_value = [&](size_t i, size_t j, uint64_t repeat) {
+    uint8_t **dpuBuffer = new uint8_t *[nrOfDPUs];
+
+    for (int i = 0; i < nrOfDPUs; i++) {
+        // void *ptr = malloc(MaxBufferSizePerDPU);
+        // aligned_alloc(1l << 21, MaxBufferSizePerDPU);
+        // madvise(ptr, MaxBufferSizePerDPU, MADV_NOHUGEPAGE);
+        // dpuBuffer[i] = (uint8_t *)ptr;
+        dpuBuffer[i] = buffer + i * MaxBufferSizePerDPU;
+    }
+
+    auto get_value = [&](size_t i, size_t j, uint64_t repeat) -> uint64_t {
         assert(i < (1 << 12));
         assert(j < (1 << 30));
         assert(repeat < (1 << 20));
         uint64_t combine = (i << 50) | (j << 20) | repeat;
         uint64_t val = parlay::hash64(combine);
-        uint8_t ret = (uint8_t)(val & 0xff);
-        return ret;
+        return val;
     };
 
-    internal_timer timer;
-    for (size_t bufferSizePerDPU = MinBufferSizePerDPU;
-         bufferSizePerDPU <= MaxBufferSizePerDPU;
-         bufferSizePerDPU <<= 1) {
-        timer.reset();
-        for (size_t repeat = 0; true; repeat++) {
+    auto CheckFlushCache = [&](bool flush_cache, size_t size) {
+        if (!flush_cache) {
+            return;
+        }
+        while (size % cacheline_size != 0) {
+            size++;
+        }
+        parlay::parallel_for(0, nrOfDPUs, [&](size_t dpu_id) {
+            for (size_t i = 0; i < size; i += cacheline_size) {
+                __builtin_ia32_clflushopt(dpuBuffer[dpu_id] + i);
+            }
+        });
+    };
 
+    internal_timer send_timer, recv_timer, total_timer;
+    for (size_t bufferSizePerDPU = MinTestSizePerDPU;
+         bufferSizePerDPU <= MaxTestSizePerDPU; bufferSizePerDPU <<= 1) {
+        send_timer.reset();
+        recv_timer.reset();
+        total_timer.reset();
+        total_timer.start();
+        assert(bufferSizePerDPU % 4 == 0);
+
+        for (size_t repeat = 0; true; repeat++) {
             parlay::parallel_for(0, nrOfDPUs, [&](size_t i) {
-                parlay::parallel_for(0, bufferSizePerDPU, [&](size_t j) {
-                    dpuBuffer[i][j] = get_value(i, j, repeat);
+                parlay::parallel_for(0, bufferSizePerDPU / 8, [&](size_t j) {
+                    uint64_t* ptr = (uint64_t*)(dpuBuffer[i] + j * 8);
+                    *ptr = get_value(i, j, repeat);
+                    // dpuBuffer[i][j] = get_value(i, j, repeat);
                 });
             });
 
-            if(direction == CommunicationDirection::Host2PIM) {
-                timer.start();
-            }
+            CheckFlushCache(flush_cache, bufferSizePerDPU);
+
+            send_timer.start();
             // CPU -> PIM.MRAM : Supported by both direct and UPMEM interface.
             interface->SendToPIM(dpuBuffer, DPU_MRAM_HEAP_POINTER_NAME, 0,
                                  bufferSizePerDPU, false);
-            if(direction == CommunicationDirection::Host2PIM) {
-                timer.end();
-            }
+            
+            send_timer.end();
 
             interface->Launch(false);
+            parlay::parallel_for(0, nrOfDPUs, [&](size_t i) {
+                parlay::parallel_for(0, bufferSizePerDPU / 8, [&](size_t j) {
+                    uint64_t* ptr = (uint64_t*)(dpuBuffer[i] + j * 8);
+                    *ptr = get_value(i, j, repeat + 1);
+                });
+            });
+            
+            CheckFlushCache(flush_cache, bufferSizePerDPU);
 
-            if (direction == CommunicationDirection::PIM2Host) {
-                timer.start();
-            }
+            recv_timer.start();
             interface->ReceiveFromPIM(dpuBuffer, DPU_MRAM_HEAP_POINTER_NAME, 0,
                                       bufferSizePerDPU, false);
-            if (direction == CommunicationDirection::PIM2Host) {
-                timer.end();
-            }
+            recv_timer.end();
 
             parlay::parallel_for(0, nrOfDPUs, [&](size_t i) {
-                parlay::parallel_for(0, bufferSizePerDPU, [&](size_t j) {
-                    assert(dpuBuffer[i][j] == get_value(i, j, repeat));
+                parlay::parallel_for(0, bufferSizePerDPU / 8, [&](size_t j) {
+                    uint64_t* ptr = (uint64_t*)(dpuBuffer[i] + j * 8);
+                    assert(*ptr == get_value(i, j, repeat));
                 });
             });
 
-            if (timer.total_time > timeLimitPerTest) {
-                double bandwidthPerDPU =
-                    (double)bufferSizePerDPU * repeat / timer.total_time;
-                double bandwidth = bandwidthPerDPU * nrOfDPUs;
+            if ((send_timer.total_time >= timeLimitPerTest &&
+                 recv_timer.total_time >= timeLimitPerTest) ||
+                (send_timer.total_time >= earlyStopTimeLimitPerTest &&
+                 recv_timer.total_time >= earlyStopTimeLimitPerTest &&
+                 repeat >= repeatLimitPerTest)) {
+                total_timer.end();
 
+                double send_bandwidth = (double)bufferSizePerDPU * repeat *
+                                        nrOfDPUs / send_timer.total_time;
+                double receive_bandwidth = (double)bufferSizePerDPU * repeat *
+                                           nrOfDPUs / recv_timer.total_time;
 
-                cout << "Buffer size: " << setw(10) << bufferSizePerDPU
-                     << " bytes, Repeat: " << setw(10) << repeat
-                     << ", Time: " << setw(10) << timer.total_time
-                     << " seconds, Bandwidth per DPU: " << setw(20) << bandwidthPerDPU
-                     << " seconds, Total Bandwidth: " << setw(20) << bandwidth
-                     << " bytes/second" << endl;
+                printf(
+                    "CLFLUSH: %s, Total Buffer size: %5lu KB, Test Buffer "
+                    "Size: %5lu KB, Repeat: %6lu, Send Time: %5lf s, Recv "
+                    "Time: %5lf s, Total Time: %5lf s, "
+                    "Send BW: %5lf GB/s, Recv BW: %5lf GB/s\n",
+                    flush_cache ? "T" : "F", MaxBufferSizePerDPU / 1024,
+                    bufferSizePerDPU / 1024, repeat, send_timer.total_time,
+                    recv_timer.total_time, total_timer.total_time,
+                    send_bandwidth / 1024.0 / 1024.0 / 1024.0,
+                    receive_bandwidth / 1024.0 / 1024.0 / 1024.0);
 
                 break;
             }
         }
     }
 
-    for (int i = 0; i < nrOfDPUs; i++) {
-        delete[] dpuBuffer[i];
-    }
     delete[] dpuBuffer;
+    free(buffer);
 }
 
 int main(int argc, char **argv) {
     int nr_ranks;
     string interfaceType;
-    CommunicationDirection direction;
 
-    parse_arguments(argc, argv, nr_ranks, interfaceType, direction);
+    parse_arguments(argc, argv, nr_ranks, interfaceType);
 
     // To Allocate: identify the number of RANKS you want, or use
     // DPU_ALLOCATE_ALL to allocate all possible.
@@ -197,7 +206,8 @@ int main(int argc, char **argv) {
     pimInterface->Launch(false);
     pimInterface->PrintLog([](int i) { return (i % 100) == 0; });
 
-    TestMRAMThroughput(pimInterface, direction);
+    TestMRAMThroughput(pimInterface, (6400 - 4) << 10, false, false);
+    TestMRAMThroughput(pimInterface, (6400 - 4) << 10, true, false);
 
     for (int i = 0; i < nrOfDPUs; i++) {
         delete[] dpuIDs[i];
